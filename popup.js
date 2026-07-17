ae57b73232b24858f4baf451bd7ce8d3c3b5b20b
김보경 <rachelkim267@naver.com>
2026-07-17 17:25:17 +0900

    Campus Hub 확장 프로그램 초기 버전

---
+++ popup.js
@@ -0,0 +1,35 @@
+const siteList = document.getElementById("site-list");
+
+sites.forEach(site => {
+
+    const card = document.createElement("div");
+    card.className = "site-card";
+
+    const wrapper = document.createElement("div");
+    wrapper.className = "icon-wrapper";
+
+    const img = document.createElement("img");
+
+    const text = document.createElement("span");
+    text.textContent = site.name;
+
+    if (site.icon) {
+        img.src = chrome.runtime.getURL(site.icon);
+    } else {
+        const domain = new URL(site.url).hostname;
+        img.src = `https://www.google.com/s2/favicons?domain=${domain}&sz=64`;
+    }
+
+    card.addEventListener("click", () => {
+        chrome.tabs.create({ url: site.url });
+    });
+
+    wrapper.appendChild(img);  
+    card.appendChild(wrapper);
+    card.appendChild(text);
+
+    siteList.appendChild(card);
+
+});
+
+
---
+++ sites.js
@@ -0,0 +1,65 @@
+const sites = [
+
+    {
+        name:"링커리어",
+        url:"https://linkareer.com"
+    },
+
+    {
+        name:"캠퍼스픽",
+        url:"https://www.campuspick.com"
+    },
+
+    {
+        name:"위비티",
+        url:"https://www.wevity.com"
+    },
+
+    {
+        name:"씽굿",
+        url:"https://www.thinkcontest.com/"
+    },
+
+    {
+        name:"올콘",
+        url:"https://www.all-con.co.kr/"
+    },
+
+    {
+        name:"씽유",
+        url:"https://thinkyou.co.kr/"
+    },
+
+    {
+        name:"슥삭",
+        url:"https://www.ssgsag.kr/"
+    },
+
+    {
+        name:"콘테스트코리아",
+        url:"https://www.contestkorea.com/"
+    },
+
+    {
+        name:"요즘것들",
+        url:"https://www.allforyoung.com/"
+    },
+
+    {
+        name:"슈퍼루키",
+        url:"https://www.superookie.com/activities"
+    },
+
+    {
+        name:"스펙업",
+        url:"https://cafe.naver.com/specup",
+        icon : "specup_icon.png"
+    },
+
+    {
+        name:"잡코리아",
+        url:"https://www.jobkorea.co.kr"
+    }
+
+
+];
